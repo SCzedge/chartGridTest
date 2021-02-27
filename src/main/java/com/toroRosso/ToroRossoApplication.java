dfeb4c3350b1b852f31f333c92ec194d1081df0c
SCzedge <sczedge@gmail.com>
2021-02-28 00:28:03 +0900

    chart and grid test project - toroRosso initial

---
+++ src/main/java/com/toroRosso/ToroRossoApplication.java
@@ -0,0 +1,13 @@
+package com.toroRosso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToroRossoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToroRossoApplication.class, args);
+	}
+
+}
